4df04d75a378fd56b5c026c02dd41cd2f9e78835
Korey <smithrkorey@gmail.com>
2021-02-06 19:59:57 -0700

    edits to server.js

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+//dependencies
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+//initalizing express
 const app = express();
 
+//middleware
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+//connecting to local database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+//routes
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
 
