92209207365db57b9f59f62cbf27da680150ef74
Korey <smithrkorey@gmail.com>
2021-02-06 19:32:51 -0700

    new model

--- models/workout.js
+++ models/workout.js
@@ -4,6 +4,37 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
   day: {
-
-    
+    type: Date,
+    default: Date.now
   },
+  exercises: {
+    type: {
+        type: String,
+        required: "Enter an exercise type."
+        },
+    name: {
+        type: String,
+        required: "Enter an exercise name."
+    },
+    duration: {
+        type: Number,
+        required: "Enter an exercise duration in minutes."
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
